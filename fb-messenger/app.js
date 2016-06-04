1399d82773c7558bd981ea7725f7f0ea2ff47513
Chris Pesto <christopher.pesto@gmail.com>
2016-06-04 15:25:05 -0700

    Adding first webhook

--- fb-messenger/app.js
+++ fb-messenger/app.js
@@ -1,8 +1,18 @@
 let express = require('express'),
     app = express();
 
+let VERIFY_TOKEN = process.env.FB_MESSENGER_VERIFY_TOKEN;
+
 app.get('/', (req, res) => {
   res.send('This provides the endpoints for the Civic Hack Zika FB Messenger UI.');
+});
+
+app.get('/webhook/', (req, res) => {
+  let result = 'Error, wrong validation token';
+  if (VERIFY_TOKEN && req.query['hub.verify_token'] === VERIFY_TOKEN) {
+    result = req.query['hub.challenge'];
+  }
+  res.send(result);
 });
 
 let server = app.listen(process.env.PORT || 3000, '0.0.0.0', () => {
