a21d8328420578c7d40d924f235fb3be9e935b77
Chris Pesto <christopher.pesto@gmail.com>
2016-06-04 17:23:31 -0700

    Updates

--- fb-messenger/app.js
+++ fb-messenger/app.js
@@ -1,18 +1,29 @@
 const request = require('request'),
       express = require('express'),
-      app = express();
+      bodyParser = require('body-parser');
 
-let VERIFY_TOKEN = process.env.FB_MESSENGER_VERIFY_TOKEN;
-var PAGE_ACCESS_TOKEN = process.env.FB_MESSENGER_PAGE_ACCESS_TOKEN;
+const app = express();
 
-const sendTextMessage = (sender, text) => {
+const VERIFY_TOKEN = process.env.FB_MESSENGER_VERIFY_TOKEN;
+const PAGE_ACCESS_TOKEN = process.env.FB_MESSENGER_PAGE_ACCESS_TOKEN;
+
+const PAYLOADS = {
+  TOP_LEVEL_LEARN: 'TOP_LEVEL_LEARN',
+  TOP_LEVEL_FIND: 'TOP_LEVEL_FIND',
+  TOP_LEVEL_NEWS: 'TOP_LEVEL_NEWS'
+};
+
+const linkButton = (title, url) => ({ 'type': 'web_url', url, title });
+const postbackButton = (title, payload) => ({ 'type': 'postback', title, payload });
+
+const sendMessage = (sender, message) => {
   request({
     url: 'https://graph.facebook.com/v2.6/me/messages',
-    qs: {access_token: PAGE_ACCESS_TOKEN},
+    qs: { access_token: PAGE_ACCESS_TOKEN },
     method: 'POST',
     json: {
-      recipient: {id: sender},
-      message: {text},
+      recipient: { id: sender },
+      message,
     }
   }, (error, response, body) => {
     if (error) {
@@ -23,9 +34,47 @@ const sendTextMessage = (sender, text) => {
   });
 };
 
+const sendTextMessage = (sender, text) => sendMessage(sender, { text });
+
+const sendButtonsMessage = (sender, text, buttons) => {
+  sendMessage(sender, {
+    attachment: {
+      type: 'template',
+      payload: { 'template_type': 'button', text, buttons }
+    }
+  });
+};
+
+const handleIncomingText = (sender, text) => {
+  switch (text) {
+    case PAYLOADS.TOP_LEVEL_LEARN:
+      sendButtonsMessage('Learn about Zika', [
+        postbackButton('What are symptoms?', ''),
+        postbackButton('How to prevent?', ''),
+        postbackButton('What are risks?', ''),
+        postbackButton('How does it spread?', ''),
+        postbackButton('How did it appear?', ''),
+      ]);
+      break;
+    case PAYLOADS.TOP_LEVEL_FIND:
+      sendButtonsMessage('Where is Zika?', [
+        linkButton('Map of cases', 'https://dfranquesa.shinyapps.io/Civic-hack-zika/')
+      ]);
+      break;
+    case PAYLOADS.TOP_LEVEL_NEWS:
+      sendButtonsMessage('Latest news', [
+        linkButton('Search Twitter', 'https://twitter.com/search?q=Zika')
+      ]);
+      break;
+  }
+};
+
 /*
  * Webhooks API endpoints
  */
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get('/', (req, res) => {
   res.send('This provides the endpoints for the Civic Hack Zika FB Messenger UI.');
@@ -46,8 +95,7 @@ app.post('/fb-messenger-webhook/', (req, res) => {
     let event = req.body.entry[0].messaging[i],
         sender = event.sender.id;
     if (event.message && event.message.text) {
-      let text = event.message.text;
-      sendTextMessage(sender, text);
+      handleIncomingText(sender, event.message.text);
     }
   }
 
